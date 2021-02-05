153bd109320c6be245fe28ebd225b193acb7afb8
JoshuaThotsana <mabotsathotsana@gmail.com>
2021-02-05 22:24:25 +0200

    Finished our loco framework

---
+++ LocoFramework/LocoFramework/Service.swift
@@ -0,0 +1,17 @@
+//
+//  Service.swift
+//  LocoFramework
+//
+//  Created by Thotsana Mabotsa on 2021/02/05.
+//
+
+import Foundation
+
+public class Service {
+	
+	private init() {}
+	
+	public static func doSomething() -> String {
+		return "Did some Stuff, here you go home boy!"
+	}
+}
